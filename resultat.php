25c1fdc05bd5a76dad46177bfc10fbcbc87f92ba
FatoumataCissoko <144293052+FatoumataCissoko@users.noreply.github.com>
2023-10-09 20:37:34 -0400

    init

--- resultat.php
+++ resultat.php
@@ -1,7 +1,6 @@
 <?php 
 // Permet d'afficher le resultat de la function.php
 require_once("function.php");
-var_dump($_POST);
 if($_POST){
     $mdp = $_POST['fname'];
     if (empty($mdp)){
@@ -13,9 +12,5 @@ if($_POST){
         echo "</br>";
         echo $mdpValid;
     }
-$mdpLengthIsvalid = mdpLengthIsvalid($_POST['fname']);
-echo '</br>';
-$saltedMdp = addSalt($_POST['fname']);
-var_dump($saltedMdp);
 }
  ?>
